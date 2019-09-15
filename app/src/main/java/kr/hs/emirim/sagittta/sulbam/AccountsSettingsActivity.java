c0543f5c0ae294a741f4977b729fe8b3fd3fd74e
ovoHJ <s2018w01@gmail.com>
2019-09-16 01:28:31 +0900

    create AccountsSettingsActivity

---
+++ app/src/main/java/kr/hs/emirim/sagittta/sulbam/AccountsSettingsActivity.java
@@ -0,0 +1,15 @@
+package kr.hs.emirim.sagittta.sulbam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountsSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accounts_settings);
+
+    }
+}
