6e4fc01a77d05d7d89d4da93f6cf70e85058cf43
ovoHJ <s2018w01@gmail.com>
2019-09-16 16:24:33 +0900

    add profile view

--- app/src/main/java/kr/hs/emirim/sagittta/sulbam/AccountsSettingsActivity.java
+++ app/src/main/java/kr/hs/emirim/sagittta/sulbam/AccountsSettingsActivity.java
@@ -2,7 +2,13 @@ package kr.hs.emirim.sagittta.sulbam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class AccountsSettingsActivity extends AppCompatActivity {
 
@@ -10,6 +16,16 @@ public class AccountsSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts_settings);
+
+        // 원형 프로필
+        ImageView profile = (ImageView) findViewById(R.id.profile);
+        profile.setBackground(new ShapeDrawable(new OvalShape()));
+        profile.setBackground(new ShapeDrawable(new OvalShape()));
+        if(Build.VERSION.SDK_INT >= 21) {
+            profile.setClipToOutline(true);
+        }
+
+        // 프로필 배경
 
     }
 }
