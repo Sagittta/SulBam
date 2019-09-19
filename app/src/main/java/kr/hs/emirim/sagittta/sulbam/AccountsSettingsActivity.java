4e66c03cb724b5943a59b96500f00ee7976aa9dc
ovoHJ <s2018w01@gmail.com>
2019-09-19 15:48:57 +0900

    add id, pwd, age, gender, email, secession view

--- app/src/main/java/kr/hs/emirim/sagittta/sulbam/AccountsSettingsActivity.java
+++ app/src/main/java/kr/hs/emirim/sagittta/sulbam/AccountsSettingsActivity.java
@@ -1,31 +1,61 @@
 package kr.hs.emirim.sagittta.sulbam;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
+import android.hardware.biometrics.BiometricManager;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class AccountsSettingsActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE = 0;
+    private ImageView profile;
+    private TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts_settings);
 
-        // 원형 프로필
-        ImageView profile = (ImageView) findViewById(R.id.profile);
-        profile.setBackground(new ShapeDrawable(new OvalShape()));
+        profile = (ImageView) findViewById(R.id.profile);
+        name = (TextView) findViewById(R.id.name);
+
+        // 프로필 원형으로 만들기
         profile.setBackground(new ShapeDrawable(new OvalShape()));
         if(Build.VERSION.SDK_INT >= 21) {
             profile.setClipToOutline(true);
         }
 
-        // 프로필 배경
-
+        name.setText("홍길동");
     }
 }
