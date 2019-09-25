837275cfaedffacd3aaf2974015d96decdbef916
Sagittta <js1verj@gmail.com>
2019-09-25 11:36:55 +0900

    make SplashActivity and layout

---
+++ app/src/main/java/kr/hs/emirim/sagittta/sulbam/SplashActivity.java
@@ -0,0 +1,27 @@
+package kr.hs.emirim.sagittta.sulbam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 3000);
+    }
+}
