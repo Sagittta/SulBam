6a57226aef5a7452875ed171078819e8bf3965cc
KangEunseo <jbogry7@gmail.com>
2019-09-25 22:11:25 +0900

    1st Feature Implementation

--- app/src/main/java/kr/hs/emirim/sagittta/sulbam/MapActivity.java
+++ app/src/main/java/kr/hs/emirim/sagittta/sulbam/MapActivity.java
@@ -1,17 +1,45 @@
 package kr.hs.emirim.sagittta.sulbam;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import kr.hs.emirim.sagittta.sulbam.Adapter.ListViewAdapter;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MapActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ListViewAdapter adapter;
+    int i;
+    private int[] img = {R.drawable.one,R.drawable.two,R.drawable.three, R.drawable.four};
+    private String[] Title = {"지민이와 사랑에 빠진 개구리","탈미림_부자","집가자 얘들아", "지민이 세컨드"};
+    private String[] Context = {"야시장 너무 노잼~~~ 돈이 없어서 노잼이었음~~~","sじゃんありがとうございました~~~~~\n"+
+            "ゃんありがとうござざいまし~~~§★♥＊§","야시장 가지말고 집 가라\n" +
+            "발 닦고 잠이나 자라", "웅냥냥힝냐링~"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        listView =(ListView)findViewById(R.id.listView);
+
+        adapter = new ListViewAdapter();    //변수 초기화
+        listView.setAdapter(adapter);   //어뎁터 할당
+
+        //adapter를 통한 값 전달
+        for(int i=0; i<img.length;i++){
+            adapter.addVO(ContextCompat.getDrawable(this,img[i]),Title[i],Context[i]);
+            adapter.notifyDataSetChanged();
+        }
+
     }
 }
--- app/src/main/java/kr/hs/emirim/sagittta/sulbam/SettingsActivity.java
+++ app/src/main/java/kr/hs/emirim/sagittta/sulbam/SettingsActivity.java
@@ -2,6 +2,7 @@ package kr.hs.emirim.sagittta.sulbam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,20 +15,31 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        TextView tx = findViewById(R.id.textView8);
-        ImageView iv = findViewById(R.id.imageView2);
-        tx.setOnClickListener(click);
-        iv.setOnClickListener(click);
+        TextView lt = findViewById(R.id.langTitle);
+        TextView lc = findViewById(R.id.langContext);
+        ImageView li = findViewById(R.id.langImg);
+        lt.setOnClickListener(click);
+        lc.setOnClickListener(click);
+        li.setOnClickListener(click);
+
+        TextView at = findViewById(R.id.accountTitle);
+        ImageView ai = findViewById(R.id.accountImg);
+        at.setOnClickListener(click);
+        ai.setOnClickListener(click);
     }
 
     View.OnClickListener click = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            //kkhjk
             switch (view.getId()){
-                case R.id.textView8:
-                case R.id.imageView2:
-                    //hhhhh
+                case R.id.langTitle:
+                case R.id.langContext:
+                case R.id.langImg:
+                    break;
+                case R.id.accountImg:
+                case R.id.accountTitle:
+                 //   Intent intent = new Intent(getApplicationContext(), AccountSettingsActivity.class);
+                 //   startActivity(intent);
                     break;
             }
         }
